f8ac80712b9389bd2b236dbb9d0b9b7935e8c32b
Derek Gumb <66969598+derekgumb@users.noreply.github.com>
2020-07-04 14:23:11 -0400

    Update Program.py
    Re-formatted code

--- Program.py
+++ Program.py
@@ -1,9 +1,9 @@
-string = input("Enter a sentence here: ")
+string = input("Enter a sentence or word here: ")
 final_string = ""
 
 for idx, char in enumerate(string):
-    if (idx+1) % 2 ==0:
-        final_string+= char.upper()
+    if (idx+1) % 2 == 0:
+        final_string += char.upper()
     else:
         final_string += char
 
